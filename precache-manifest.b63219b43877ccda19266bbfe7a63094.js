8f4d04d5aad3e5bf431102e49423ba70f9996d8f
Nasreen Sayed Siddig Elhussain <nasreen.store@gmail.com>
2019-10-25 04:15:56 +0400

    Updates

---
+++ precache-manifest.b63219b43877ccda19266bbfe7a63094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d515145c42bbdebafde416d1e8711cd4",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5245d1dae03be7601670",
+    "url": "/pomodoro-clock/static/css/main.d47ebc3a.chunk.css"
+  },
+  {
+    "revision": "4300398e12db3f39c742",
+    "url": "/pomodoro-clock/static/js/2.62050b00.chunk.js"
+  },
+  {
+    "revision": "5245d1dae03be7601670",
+    "url": "/pomodoro-clock/static/js/main.d95898f3.chunk.js"
+  },
+  {
+    "revision": "07ad79a1018402bfcb07",
+    "url": "/pomodoro-clock/static/js/runtime-main.4953bf39.js"
+  },
+  {
+    "revision": "238acd50a6dd935336d61e14235449bf",
+    "url": "/pomodoro-clock/static/media/beep1.238acd50.mp3"
+  }
+]);
